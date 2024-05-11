c8bc1c254629b141dbf41bbbbf2c503a5c592408
exisodd <ethantran.mail@gmail.com>
2024-05-11 12:54:47 -0700

    add search bar

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,7 +1,8 @@
 import { StyleSheet } from "react-native";
+// import SearchBar from "../components/SearchBar";
 
 export default function CameraScreen() {
-    
+    // <SearchBar />
 };
 
 const styles = StyleSheet.create({
