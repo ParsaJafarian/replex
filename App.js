6d28e0c0c39b4ba9d1dbf34235b9bf7a40cafaf1
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-05-11 14:51:56 -0400

    Add basic navigation

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import CameraScreen from './screens/CameraScreen';
+import WorkoutScreen from './screens/WorkoutScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <>
+    <StatusBar style="light" />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Camera" component={CameraScreen} />
+        <Tab.Screen name="Workout" component={WorkoutScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  </>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/CameraScreen.js
@@ -0,0 +1,7 @@
+export default function CameraScreen() {
+    
+};
+
+const styles = StyleSheet.create({
+    
+});
