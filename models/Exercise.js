64bd740776bb9554bab28b38f0d5447c08bdc438
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-05-11 14:25:40 -0400

    Add exercise class and data

---
+++ models/Exercise.js
@@ -0,0 +1,12 @@
+export default class Exercise {
+    //@TODO add AI model conditions
+    /**
+     * @param {string} id 
+     * @param {string} name 
+     */
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        //this.conditions = conditions;
+    }
+}
