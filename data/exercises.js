825df98334a0aca2765a661ee799d3bc383d459b
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-05-12 02:18:20 -0400

    Remove models folder and migrate Exercise class to data/exercises.js

--- data/exercises.js
+++ data/exercises.js
@@ -1,16 +1,13 @@
-import Exercise from "../models/Exercise";
+export class Exercise {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
 
 export const EXERCISES = [
     new Exercise('e1', 'Pushups'),
-    new Exercise('e2', 'Bench Press'),
-    new Exercise('e3', 'Chest Fly'),
-    new Exercise('e4', 'Pullups'),
-    new Exercise('e5', 'Deadlift'),
-    new Exercise('e6', 'Bent Over Row'),
-    new Exercise('e7', 'Plank'),
-    new Exercise('e8', 'Russian Twist'),
-    new Exercise('e9', 'Leg Raise'),
-    // new Exercise('e10', 'Squat'),
-    // new Exercise('e11', 'Lunges'),
-    // new Exercise('e12', 'Leg Press'),
+    new Exercise('e2', 'Leg Raise'),
+    new Exercise('e3', 'Squats'),
+    new Exercise('e4', 'Crunches'),
 ]
--- models/Exercise.js
+++
@@ -1,7 +0,0 @@
-export default class Exercise {
-    //@TODO add AI model attribute
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-}
